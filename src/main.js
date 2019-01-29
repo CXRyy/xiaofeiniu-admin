7347c652d8e8772cf55240b6d872d4bd1267b2dd
CXRyy <18801274409@163.com>
2019-01-29 18:05:48 +0800

    admin组件

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import Qrcode from 'qrcode'
+
+Vue.use(ElementUI)
+Vue.use(Qrcode)
+
+Vue.config.productionTip = false
+
+Vue.prototype.$axios=axios
+
+Vue.filter("date",(val)=>{
+  var date=new Date(val)
+  var y=date.getFullYear() 
+  var m=date.getMonth()+1
+  m>9?m:"0"+m
+  var d=date.getDate()
+  d>9?d:"0"+d
+  return y+"-"+m+"-"+d
+})
+Vue.filter("datatime",(val)=>{
+  var date=new Date(val)
+  var y=date.getFullYear() 
+  var m=date.getMonth()+1
+  m>9?m:"0"+m
+  var d=date.getDate()
+  d>9?d:"0"+d
+  var h=date.getHours()
+  h>9?h:"0"+h
+  var minute=date.getMinutes()
+  minute>9?minute:"0"+minute
+  var s=date.getSeconds()
+  s>9?s:"0"+s
+  return y+"-"+m+"-"+d+" "+h+":"+minute+":"+s
+})
+Vue.filter("currency",(val)=>{
+  return "¥"+val.toFixed(2)
+})
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
